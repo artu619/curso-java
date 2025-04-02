0a3f79ec6bab1f54f12724873964662aca97da05
artu619 <arturolp60@gmail.com>
2025-04-02 12:33:37 +0200

    cosas

---
+++ trabajos/I_AnalisisTexto.java
@@ -0,0 +1,12 @@
+/*
+ * Haz un programa que reciba un texto
+ * Divide el texto en palabras
+ * Muestra el total de palabras
+ * ¿Cuál es la palabra más larga?
+ */
+
+public class I_AnalisisTexto {
+    public static void main(String[] args) {
+        
+    }
+}
